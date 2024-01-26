125a53bfa15516936db8fa04ddb8af572cab4726
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-26 11:28:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateContent() {
+  // Get the p element with the id "status"
+  const statusP = document.getElementById("status");
+
+  
+  const newContent = document.createElement("h1");
+  newContent.textContent = "Entered Metaverse";
+
+  statusP.innerHTML = ""; 
+  statusP.appendChild(newContent); 
+}
